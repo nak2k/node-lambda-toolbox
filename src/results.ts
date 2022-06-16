897a7606463c1e482dea06a519beb46539cd49ff
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2022-06-17 00:38:56 +0900

    Modify results

--- src/results.ts
+++ src/results.ts
@@ -1,29 +1,29 @@
 import { APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
 import { STATUS_CODES } from 'http';
 
-export function result301(location: string): APIGatewayProxyStructuredResultV2 {
+export function result301(location: string | URL): APIGatewayProxyStructuredResultV2 {
   return {
     statusCode: 301,
     headers: {
-      location,
+      location: location as string,
     },
   };
 }
 
-export function result302(location: string): APIGatewayProxyStructuredResultV2 {
+export function result302(location: string | URL): APIGatewayProxyStructuredResultV2 {
   return {
     statusCode: 302,
     headers: {
-      location,
+      location: location as string,
     },
   };
 }
 
-export function result303(location: string): APIGatewayProxyStructuredResultV2 {
+export function result303(location: string | URL): APIGatewayProxyStructuredResultV2 {
   return {
     statusCode: 303,
     headers: {
-      location,
+      location: location as string,
     },
   };
 }
@@ -39,6 +39,8 @@ function resultOf(statusCode: number): APIGatewayProxyStructuredResultV2 {
 }
 
 export const result400: APIGatewayProxyStructuredResultV2 = resultOf(400);
+export const result401: APIGatewayProxyStructuredResultV2 = resultOf(401);
+export const result403: APIGatewayProxyStructuredResultV2 = resultOf(403);
 export const result404: APIGatewayProxyStructuredResultV2 = resultOf(404);
 export const result405: APIGatewayProxyStructuredResultV2 = resultOf(405);
 
@@ -46,5 +48,7 @@ export const resultMovedPermanently = result301;
 export const resultFound = result302;
 export const resultSeeOther = result303;
 export const resultBadRequest = result400;
+export const resultUnauthorized = result401;
+export const resultForbbiden = result403;
 export const resultNotFound = result404;
 export const resultMethodNotAllowed = result405;
